c80f1dc0247aede6d59c3c9d05a8ebb83d8d1b1b
Trox <gab_83000@hotmail.fr>
2012-10-21 03:21:55 +0200

    testgab avec sauvegarde

--- Testgab/rotation.ml
+++ Testgab/rotation.ml
@@ -30,6 +30,96 @@ let rec wait_key () =
        Sdlevent.KEYDOWN _ -> ()
       | _ -> wait_key ()
 
+let dim surface =
+  Sdlvideo.surface_dims surface
+
+let mean_of_float x = match (modf x) with
+  | (x,y) when (x >= 0.5)-> int_of_float(floor(y +. 1.))
+  | (_,y) -> int_of_float(floor(y))
+
+let center surface =
+  let (width,height,pitch) = dim surface in
+    match (width,height) with
+      |(x,y) -> (x / 2,y / 2)
+
+let hypotenuse surface =
+  let (width,height,pitch) = Sdlvideo.surface_dims surface in
+  let  hyp = sqrt(float_of_int(height*height + width*width)) in
+   mean_of_float hyp
+
+let tab width height = (Bigarray.Array2.create
+                                Bigarray.int32
+                                Bigarray.c_layout
+                                width
+                                height)
+
+let img_to_matrice img =
+ let (width,height,pitch) = Sdlvideo.surface_dims img
+        in let my_matrice =
+        (Bigarray.Array2.create
+        Bigarray.int32
+        Bigarray.c_layout
+        width
+        height) in
+for j=0 to (height - 1) do
+  for i=0 to (width - 1) do
+   Bigarray.Array2.set my_matrice i j (Sdlvideo.get_pixel img i j)
+ done;
+ done;
+my_matrice
+
+let direct_int i j theta=
+  let x = float_of_int i in
+  let y = float_of_int j in
+  let x' =    x*.cos(theta) -. y*.sin(theta) in
+  let y' =    x*.sin(theta) +. y*.cos(theta) in
+    (mean_of_float x', mean_of_float y')
+
+let matrice_to_img matrice =
+  let width = Bigarray.Array2.dim1 matrice in
+  let height = Bigarray.Array2.dim2 matrice in
+  let rm = Int32.of_int(255) in
+  let gm = Int32.of_int(255) in
+  let bm = Int32.of_int(255) in
+  let am = Int32.of_int(255) in
+  let my_img = Sdlvideo.create_RGB_surface
+    [`SWSURFACE;`SRCCOLORKEY;`SRCALPHA]
+    ~w:width
+    ~h:height
+    ~bpp:32
+    ~rmask:rm
+    ~gmask:gm
+    ~bmask:bm
+    ~amask:am in
+for j=0 to (height - 1) do
+for i=0 to (width - 1) do
+  Sdlvideo.put_pixel my_img i j (Bigarray.Array2.get matrice i j)
+done;
+done;
+my_img
+
+let rotation img angle =
+  let (height,width,pitch) = dim img in
+  let (cy,cx) = center img in
+  let cote_hypot = hypotenuse img  in
+  let demi_hypot = cote_hypot / 2 in
+  let my_output = tab cote_hypot cote_hypot in
+  let my_input = img_to_matrice img in
+    for i=0 to (width - 1) do
+      for j=0 to (height - 1) do
+        let (x,y) = direct_int (i - cx) (j - cy) angle in
+            Bigarray.Array2.set
+                my_output
+                ( y + demi_hypot )
+                ( x + demi_hypot )
+                (Bigarray.Array2.get
+                   my_input
+                   (j)
+                   (i));
+      done;
+    done;
+  matrice_to_img my_output
+
 let degree2radian theta =
   180. *. float_of_int theta /. 3.14159265359
 
@@ -61,7 +151,7 @@ let hough img =
 	  max <- matrix.(i).(j)
       done;
     done;
-  print_int snd max
+  snd max
 
 let main =
   begin
@@ -72,16 +162,7 @@ let main =
     sdl_init ();
     (* Chargement d'une image *)
     let img = Sdlloader.load_image Sys.argv.(1) in
-    (*(* On récupère les dimensions *)
-    let (w,h) = get_dims img in
-    (* On crée la surface d'affichage en doublebuffering *)
-    let display = Sdlvideo.set_video_mode w h [`DOUBLEBUF] in
-      (* on affiche l'image *)
-      show img display;*)
-      (* on attend une touche *)
-      wait_key();
-      hough img;
-      wait_key();
+      save (rotation img (hough img));
       (* on quitte *)
       exit 0
   end
